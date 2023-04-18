2dd25b253a0cd9aaad725929d7ca8782cd5f9882
preciousimo <preciousimoniakemu@gmail.com>
2023-04-18 14:37:06 +0100

    Generic Views in Django Rest Framework

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,5 @@ from .views import*
 
 urlpatterns = [
     path('teacher/', TeacherList.as_view(), name='teacher'),
+    path('teacher/<int:pk>/', TeacherDetail.as_view(), name='teacher_detail'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import generics
 from .serializers import*
 from .models import *
 
-# Create your views here.
-class TeacherList(APIView):
-    def get(self, request):
-        teachers = Teacher.objects.all()
-        serializer = TeacherSerializer(teachers, many=True)
-        return Response(serializer.data)
+class TeacherList(generics.ListCreateAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
