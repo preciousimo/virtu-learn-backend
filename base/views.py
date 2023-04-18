69f631c4dab4a880a9ab5d16d0946dee6d68e735
preciousimo <preciousimoniakemu@gmail.com>
2023-04-18 14:44:04 +0100

    DRF Authentication and Permissions

--- base/views.py
+++ base/views.py
@@ -1,14 +1,15 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.shortcuts import render 
 from rest_framework import generics
+from rest_framework import permissions
 from .serializers import*
 from .models import *
 
 class TeacherList(generics.ListCreateAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
+    permission_classes = [permissions.IsAuthenticated]
--- meadow_lms_backend/urls.py
+++ meadow_lms_backend/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
