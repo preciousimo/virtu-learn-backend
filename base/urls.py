af9d0ab3308dee760aa20f56f9940649ffaa56c6
preciousimo <preciousimoniakemu@gmail.com>
2023-05-03 15:56:18 +0100

    Login API

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import*
 
 urlpatterns = [
-    path('teacher/', TeacherList.as_view(), name='teacher'),
-    path('teacher/<int:pk>/', TeacherDetail.as_view(), name='teacher_detail'),
+    path('teacher/', TeacherList.as_view()),
+    path('teacher/<int:pk>/', TeacherDetail.as_view()),
+    path('teacher-login/', teacher_login),
 ]
--- base/views.py
+++ base/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render 
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework import generics
 from rest_framework import permissions
-from .serializers import*
+from .serializers import *
 from .models import *
 
 class TeacherList(generics.ListCreateAPIView):
@@ -13,3 +17,16 @@ class TeacherDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
     # permission_classes = [permissions.IsAuthenticated]
+
+@csrf_exempt
+def teacher_login(request):
+    email=request.POST.get('email')
+    password=request.POST.get('password')
+    try:
+        teacherData=Teacher.objects.get(email=email,password=password)
+        if teacherData:
+            return JsonResponse({'bool':True})
+        else:
+            return JsonResponse({'bool':False})
+    except:
+        return JsonResponse({'status':'failed','message':'Invalid Input'})
