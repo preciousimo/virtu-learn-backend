7001920350df7d71828648cf3036b4e552dab5a3
preciousimo <preciousimoniakemu@gmail.com>
2024-02-22 07:35:47 +0100

    feat: Create landing page for Virtu Learn Backend API

--- sonips_lms_backend/settings.py
+++ sonips_lms_backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
 
     'base',
+    'landing',
 ]
 
 MIDDLEWARE = [
--- sonips_lms_backend/urls.py
+++ sonips_lms_backend/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('landing.urls')),
     path('api/', include('base.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
